6e8eb3d27f9769bbade5eeb389d4a91623be96e7
Obedience-Muvirimi <obediencejima1@gmail.com>
2026-09-04 13:25:52 +0200

    I just added the end pr

--- yahtzee-scores/yahtzee.py
+++ yahtzee-scores/yahtzee.py
@@ -218,3 +218,4 @@ while len(player_scores)<13 and len(computer_scores)<13:
         computer_scores[computer_choice] = current_score[computer_choice]
         print("This is computer's scorecard :" ,computer_scores)
  # this is the print statement
+    print("---------------THE END----------------")
